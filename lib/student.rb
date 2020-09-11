d2be5356cba1374b88fca9dc01f77f0b1726df3a
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-11 01:51:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,14 +85,28 @@ class Student
      def self.first_X_students_in_grade_10(x)
        
        sql = <<-SQL
-       SELECT *
-       FROM students
-       WHERE grade = 10
-       LIMIT ?
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        LIMIT ?
        SQL
        
        DB[:conn].execute(sql, x).map do |row|
-            self.new_from_db(row)
+          self.new_from_db(row)
        end
-    end   
+    end  
+    
+    def  self.first_student_in_grade_10
+      
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+      SQL
+      
+      DB[:conn].execute(sql).map do |row|
+         self.new_from_db(row)
+      end.first
+    end
+    
 end
